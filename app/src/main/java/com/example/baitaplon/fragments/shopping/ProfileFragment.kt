829f36d5250151056085a167e897e14a41145339
= <=>
2024-05-24 18:38:18 +0700

    Log out

--- app/src/main/java/com/example/baitaplon/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/example/baitaplon/fragments/shopping/ProfileFragment.kt
@@ -1,8 +1,41 @@
 package com.example.baitaplon.fragments.shopping
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.baitaplon.R
+import com.example.baitaplon.activity.LoginRegisterActivity
+import com.example.baitaplon.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var mContext: Context
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logout.setOnClickListener {
+            val intent = Intent(mContext, LoginRegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
 }
